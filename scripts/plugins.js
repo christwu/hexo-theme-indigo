a963674f0cf964d2c2ef51446ad356873dbc0e69
christwu <38274826+christwu@users.noreply.github.com>
2018-07-15 18:00:02 +0800

    处理主题设置被覆盖后仍然获取原值的问题

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -1,17 +1,25 @@
-const { version, name } = require('../package.json')
+const { version, name } = require('../package.json');
 
-hexo.extend.helper.register('theme_version', () => version)
+hexo.extend.helper.register('theme_version', () => version);
+
+const get_config = (key) => {
+    if (hexo.config.theme_config && hexo.config.theme_config.hasOwnProperty(key)) {
+        return hexo.config.theme_config[key];
+    } else {
+        return hexo.theme.config[key];
+    }
+}
 
 const source = (path, cache, ext) => {
     if (cache) {
-        const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`
-        return hexo.theme.config.cdn ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`
+        const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`;
+        return get_config('cdn') ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`;
     } else {
-        return path + ext
+        return path + ext;
     }
 }
-hexo.extend.helper.register('theme_js', (path, cache) => source(path, cache, '.js'))
-hexo.extend.helper.register('theme_css', (path, cache) => source(path, cache, '.css'))
+hexo.extend.helper.register('theme_js', (path, cache) => source(path, cache, '.js'));
+hexo.extend.helper.register('theme_css', (path, cache) => source(path, cache, '.css'));
 
 function renderImage(src, alt = '', title = '') {
     return `<figure class="image-bubble">
@@ -20,30 +28,30 @@ function renderImage(src, alt = '', title = '') {
                     <img src="${src}" alt="${alt}" title="${title}">
                 </div>
                 <div class="image-caption">${title || alt}</div>
-            </figure>`
+            </figure>`;
 }
 
 hexo.extend.tag.register('image', ([src, alt = '', title = '']) => {
-    return hexo.theme.config.lightbox ? renderImage(src, alt, title) : `<img src="${src}" alt="${alt}" title="${title}">`
-})
+    return get_config('lightbox') ? renderImage(src, alt, title) : `<img src="${src}" alt="${alt}" title="${title}">`;
+});
 
 hexo.extend.filter.register('before_post_render', data => {
-    if (hexo.theme.config.lightbox) {
+    if (get_config('lightbox')) {
         // 包含图片的代码块 <escape>[\s\S]*\!\[(.*)\]\((.+)\)[\s\S]*<\/escape>
         // 行内图片 [^`]\s*\!\[(.*)\]\((.+)\)([^`]|$)
         data.content = data.content.replace(/<escape>.*\!\[(.*)\]\((.+)\).*<\/escape>|([^`]\s*|^)\!\[(.*)\]\((.+)\)([^`]|$)/gm, match => {
 
             // 忽略代码块中的图片
             if (/<escape>[\s\S]*<\/escape>|.?\s{3,}/.test(match)) {
-                return match
+                return match;
             }
 
             return match.replace(/\!\[(.*)\]\((.+)\)/, (img, alt, src) => {
-                const attrs = src.split(' ')
-                const title = (attrs[1] && attrs[1].replace(/\"|\'/g, '')) || ''
-                return `{% image ${attrs[0]} '${alt}' '${title}' %}`
-            })
-        })
+                const attrs = src.split(' ');
+                const title = (attrs[1] && attrs[1].replace(/\"|\'/g, '')) || '';
+                return `{% image ${attrs[0]} '${alt}' '${title}' %}`;
+            });
+        });
     }
-    return data
-})
+    return data;
+});
